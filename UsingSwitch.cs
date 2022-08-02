42bf1f8f13ea10fbd4fcae5d90411f1cb1a7a2b3
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 17:10:27 +0530

    Calculated wages using switch case for month

--- UsingSwitch.cs
+++ UsingSwitch.cs
@@ -10,7 +10,6 @@ namespace EmployeeWageUsingObject
     {
         public void CheckEmployeeAttendence()
         {
-            //Constants
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             int WAGE_PER_HOUR = 20;
@@ -20,24 +19,28 @@ namespace EmployeeWageUsingObject
             int empWage = 0;
             int totalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(3);
-
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHr = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHr = 8;
-                    break;
-                default:
-                    empHr = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHr = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                empWage = empHr * WAGE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee wage is: " + empWage);
             }
-            empWage = empHr * WAGE_PER_HOUR;
-            totalEmpWage += empWage;
-            Console.WriteLine("Employee wage is: " + empWage);
+            Console.WriteLine("Employee wage is: " + totalEmpWage);
         }
     }
 }
