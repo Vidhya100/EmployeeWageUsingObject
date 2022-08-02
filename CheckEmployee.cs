c6bb715feac2e8fb12a7c7d91a44bc7492e4d30e
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 17:00:25 +0530

    Partime employee added

--- CheckEmployee.cs
+++ CheckEmployee.cs
@@ -10,28 +10,35 @@ namespace EmployeeWageUsingObject
     {
         public void CheckEmployeeAttendence()
         {
-                //Constants
-                int IS_FULL_TIME = 1;
-                int WAGE_PER_HOUR = 20;
-                //Variables
-                int empHr = 0;
-                int empWage = 0;
+            //Constants
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int WAGE_PER_HOUR = 20;
+            //Variables
+            int empHr = 0;
+            int empWage = 0;
 
-                Random random = new Random();
-                int empCheck = random.Next(2);
+            Random random = new Random();
+            int empCheck = random.Next(3);
 
-                if (empCheck == IS_FULL_TIME)
-                {
-                    Console.WriteLine("Employee is Present");
-                    empHr = 8;
-                }
-                else
-                {
-                    Console.WriteLine("Employee is Absent");
-                    empHr = 0;
-                }
-                empWage = empHr * WAGE_PER_HOUR;
-                Console.WriteLine("Employee wage is: " + empWage);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHr = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee did Parttime.");
+                empHr = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHr = 0;
+            }
+            empWage = empHr * WAGE_PER_HOUR;
+            Console.WriteLine("Employee wage is: " + empWage);
+
         }
     }
 }
