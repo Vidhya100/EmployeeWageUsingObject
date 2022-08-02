ee5245d4ba6734be611258532a826d201a6cade4
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 17:07:20 +0530

    Calculated wages using switch case

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace EmployeeWageComputation
+﻿using EmployeeWageUsingObject;
+
+namespace EmployeeWageComputation
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employeewage program.");
+
+            //CheckEmployee checkEployee = new CheckEmployee();
+            //checkEployee.CheckEmployeeAttendence();
+
+            UsingSwitch usingSwitch = new UsingSwitch();
+            usingSwitch.CheckEmployeeAttendence();
         }
     }
 }
