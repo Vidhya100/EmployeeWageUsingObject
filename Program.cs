3cc45ca2a5f6126fb78ff5eaef9e50649caad998
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-02 16:34:40 +0530

    Greating meassage

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EmployeeWageComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+        }
+    }
+}
