9acf9b457f1ee0be1976f0d515277d2ca7baedd5
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-05 11:55:27 +0530

    Fuction called using object

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EmployeeWageComputation
+﻿using EmployeeWageUsingObject;
+
+namespace EmployeeWageComputation
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employeewage program.");
+
+            CheckEmployee checkEmployee = new CheckEmployee();
+            checkEmployee.CheckEmployeeAttendence();
         }
     }
 }
