f216d8c3ceac5063fcae5921f36c4e67b859509a
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-06 20:45:21 +0530

    Employee wage calculated for multiple companies

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EmployeeWageComputation
+﻿using EmployeeWageUsingObject;
+
+namespace EmployeeWageComputation
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employeewage program.");
+
+            MultipleCompanies.ComputeEmpWage("Dmart", 20, 2, 10);
+            MultipleCompanies.ComputeEmpWage("Reliance", 10, 4, 20);
         }
     }
 }
