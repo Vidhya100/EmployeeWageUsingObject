1485cc4061ed8e1e60ccf4367dfd5ae2413e5128
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-08 17:37:17 +0530

    Getting companies employee wages

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace EmployeeWageComputation
             empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
             empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
             empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total Wage for Dmart company :{0} ",empWageBuilder.getTotalWage("Dmart"));
+            Console.WriteLine("Total Wage for Reliance company :{0} ", empWageBuilder.getTotalWage("Reliance"));
+
             //EmpWageBuilder dMart = new EmpWageBuilder("Dmart", 20, 2, 10);
             //EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
 
